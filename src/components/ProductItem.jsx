f8657d9c33ea798d25ac2e73c583986bf045e540
rivaldo <leo_1998@live.cl>
2023-04-08 01:38:40 -0400

    punto de guardado

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {useReducer, useState} from 'react';
 import '../styles/ProductItem.scss';
 import AddCart from '@icons/bt_add_to_cart.svg'
 
 const ProductItem = () => {
+	const [cart,setCart] = useState('hola tio');
+	const handleCart = () => {
+		setCart('hola mundo');
+	}
 	return (
 		<div className="ProductItem">
 			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
@@ -11,9 +15,10 @@ const ProductItem = () => {
 					<p>$120,00</p>
 					<p>Bike</p>
 				</div>
-				<figure>
+				<figure onClick={handleCart}>
 					<img src={AddCart}alt="" />
 				</figure>
+				<p>{cart}</p>
 			</div>
 		</div>
 	);
